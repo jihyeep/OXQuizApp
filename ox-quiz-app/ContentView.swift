de8af53ce76d67b602b547f992285f93eb72f925
Jihye Park <deviloper.park@gmail.com>
2024-04-15 15:18:25 +0900

    Base frame

--- ox-quiz-app/ContentView.swift
+++ ox-quiz-app/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1: Int = 4
+    @State var number2: Int = 2
+    @State var resultNumber: Int = 9
+    
+    @State var countCorrect: Int = 0
+    @State var countWrong: Int = 0
+    
     var body: some View {
+        // UI 구성 및 로직 작성 부분
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
         }
-        .padding()
+        
+        // 게임 시작 로직
+        func reloadGame() {
+            
+        }
+        
+        // 정답 선택 시 로직
+        func selectCorrect() {
+            
+        }
+        
+        // 오답 선택 시 로직
+        func selectWrong() {
+            
+        }
     }
 }
 
