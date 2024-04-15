705bd068faac618d9b6151fefe0194293fc9b1b6
Jihye Park <deviloper.park@gmail.com>
2024-04-15 15:56:51 +0900

    [feat] UI structure

--- ox-quiz-app/ContentView.swift
+++ ox-quiz-app/ContentView.swift
@@ -18,23 +18,67 @@ struct ContentView: View {
     var body: some View {
         // UI 구성 및 로직 작성 부분
         VStack {
+            Text("다음 수식은 맞을까요?")
+                .padding()
+                .font(.system(size: 30))
+//            Spacer()
+            Text("\(number1) X \(number2) = \(resultNumber)")
+                .font(.system(size: 30))
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "checkmark.diamond.fill")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.green)
+                    Text("맞음")
+                        .font(.system(size: 40, weight: .bold))
+                        .foregroundStyle(Color.green)
+                }
+                .padding(.trailing)
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark.diamond")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.red)
+                    Text("틀림")
+                        .font(.system(size: 40, weight: .bold))
+                        .foregroundStyle(Color.red)
+                }
+                .padding(.leading)
+
+            }
+            HStack {
+                Text("\(countCorrect)개 맞춤")
+                Text("\(countWrong)개 틀림")
+            }
+            .font(.system(size: 30))
+            Button {
+
+            } label: {
+                Text("카운트 초기화")
+                    .font(.system(size: 30))
+            }
             
         }
         
         // 게임 시작 로직
-        func reloadGame() {
-            
-        }
+//        func reloadGame() {
+//
+//        }
         
         // 정답 선택 시 로직
-        func selectCorrect() {
-            
-        }
+//        func selectCorrect() {
+//
+//        }
         
         // 오답 선택 시 로직
-        func selectWrong() {
-            
-        }
+//        func selectWrong() {
+//            
+//        }
     }
 }
 
