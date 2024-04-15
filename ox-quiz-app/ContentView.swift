a09bc2471fd5a5375a7ab84fcacf490ba591b697
Jihye Park <deviloper.park@gmail.com>
2024-04-15 15:12:27 +0900

    Initial Commit

---
+++ ox-quiz-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ox-quiz-app
+//
+//  Created by 박지혜 on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ox-quiz-app/ox_quiz_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ox_quiz_appApp.swift
+//  ox-quiz-app
+//
+//  Created by 박지혜 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ox_quiz_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
